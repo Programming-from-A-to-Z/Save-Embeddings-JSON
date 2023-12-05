0f40be643a908ed94f2c53584d87a65293d65623
Daniel Shiffman <daniel.shiffman@gmail.com>
2023-12-05 10:45:05 -0500

    adjust code to match live stream

--- embeddings-transformers.js
+++ embeddings-transformers.js
@@ -1,44 +1,41 @@
 import * as fs from 'fs';
 import { pipeline } from '@xenova/transformers';
 
+// Load the embeddings model
+const extractor = await pipeline('feature-extraction', 'Xenova/bge-small-en-v1.5');
+
 // Read and split the transcript into chunks
 console.log('Reading source file...');
 const raw = fs.readFileSync('p5.txt', 'utf-8');
 let chunks = raw.split(/\n+/);
+
 // Trim each chunk and filter out empty strings
 chunks = chunks.map((chunk) => chunk.trim()).filter((chunk) => chunk !== '');
 console.log(`Total chunks to process: ${chunks.length}`);
 
 // Start the process of generating embeddings
 console.log('Starting embedding generation...');
-createEmbeddings(chunks);
 
-// Function to create embeddings for each chunk
-async function createEmbeddings(chunks) {
-  // Load the embeddings model
-  const bgeModel = await pipeline('feature-extraction', 'Supabase/bge-small-en');
+// Array to store all embeddings in output JSON
+const outputJSON = { embeddings: [] };
 
-  let embeddings = []; // Array to store all embeddings
+// Process chunks in batches
+for (let i = 0; i < chunks.length; i++) {
+  console.log(`Processing ${i + 1}/${Math.ceil(chunks.length)}`);
 
-  // Process chunks in batches
-  for (let i = 0; i < chunks.length; i++) {
-    console.log(`Processing batch ${i + 1}/${Math.ceil(chunks.length)}`);
+  // Generate the embedding from text
+  const output = await extractor(chunks[i], {
+    pooling: 'mean',
+    normalize: true,
+  });
+  // Extract the embedding output
+  const embedding = output.tolist()[0];
 
-    // Generate the embedding from text
-    const output = await bgeModel(chunks[i], {
-      pooling: 'mean',
-      normalize: true,
-    });
-    // Extract the embedding output
-    const embedding = Array.from(output.data);
-
-    // Add current batch embeddings
-    embeddings.push({ text: chunks[i], embedding });
-  }
-
-  const jsonOut = { embeddings };
-  // Write the embeddings to a JSON file
-  const fileOut = 'embeddings.json';
-  fs.writeFileSync(fileOut, JSON.stringify(jsonOut));
-  console.log(`Embeddings saved to ${fileOut}`);
+  // Add current batch embeddings
+  outputJSON.embeddings.push({ text: chunks[i], embedding });
 }
+
+// Write the embeddings to a JSON file
+const fileOut = 'embeddings.json';
+fs.writeFileSync(fileOut, JSON.stringify(outputJSON));
+console.log(`Embeddings saved to ${fileOut}`);
